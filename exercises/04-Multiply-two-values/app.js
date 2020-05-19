32f3ab68833ddaf73249d2d6c9b8776a759ce75b
johnrohan87 <john.rohan87@gmail.com>
2020-05-19 02:34:25 +0000

    add files

--- exercises/04-Multiply-two-values/app.js
+++ exercises/04-Multiply-two-values/app.js
@@ -1,1 +1,3 @@
 // Your code below:
+var somevar = 2345 * 7323
+console.log(somevar)
--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,5 @@
 var age = prompt('What is your age?');
 
 // Your code below:
+var tmp_age = Number(age) +10
+console.log(tmp_age)
