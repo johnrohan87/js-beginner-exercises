06a089dbdc15583695702c6752eb3dea1fafa27f
johnrohan87 <john.rohan87@gmail.com>
2020-05-24 21:47:06 +0000

    updating

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,4 +1,19 @@
-
+const sing = () => {
+    var strTmp = ""
+    for (i = 0;i < 12;i++){
+        
+        if (i == 4){
+            strTmp = strTmp + "words of wisdom, "
+        }
+        else if (i == 10){
+            strTmp = strTmp + "there will be an answer, "
+        }
+        else {
+            strTmp = strTmp + "let it be, "
+        }
+    }
+    return strTmp
+}
 
 //Your code above ^^^
 
